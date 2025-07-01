4cdb58f065adf7ff3cc439d3bcc18f6f95b4f5fe
misconceptor <thespeedcubers347@gmail.com>
2025-07-01 09:24:10 -0700

    python

--- a.py
+++ a.py
@@ -3,11 +3,7 @@ def matr():
 r,c=map(int,input().split())
 arr = [[],[]]
 for i in range(r):
-    row=[]
-    for j in range(c):
-        row=list(map(int,input().split()))
+    row=list(map(int,input().split()))
     arr.append(row)
-
-for i in range(r):
-    for j in range(c):
-        print(arr[i][j],' ','\n')
+for row in arr:
+    print(' '.join(map(str,row)))
