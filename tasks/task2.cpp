f92982817ad31b52edf499f51861dcfd352a7666
misconceptor <thespeedcubers347@gmail.com>
2025-07-09 07:04:17 -0700

    weather

--- tasks/task2.cpp
+++ tasks/task2.cpp
@@ -1,28 +1,44 @@
+#include <fstream>
 #include <iostream>
 #include <regex>
-#include <string>
 using namespace std;
-// Напишите регулярное выражение
-// для поиска в заданной строке всех подстрок,
-// удовлетворяющих шаблону:
-// буква 'a', 2 любых символа, буква 'a',
-// но пропускающих подстроку abca.
 
-int main(){
-    regex r(R"(a(?:(?!bc)..)a)");
-    string s="sdf abca aabcba";
-    auto b = sregex_iterator(s.begin(),s.end(),r);
-    auto e = sregex_iterator();
-    for(auto i = b; i!=e; ++i){
-        cout<<i->str()<<' ';
+void Temp(smatch s) {
+    int deg = stoi(s.str(8));
+    string sign = s.str(7);
+    if (sign == "-")
+        deg *= -1;
+        // deg= -deg;
+    if (deg< -20 || deg> 40 ) {
+        cout << "Invalid temperature: " << deg<< '\n';
     }
-    cout<<endl;
+}
 
-    // smatch m;
-    // while(regex_search(s,m,r)){
-    //     cout<<m.str()<<' ';
-    //     s=m.suffix().str();
-    // }
-    // cout<<endl;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    regex r(R"((.* +|)(весна|лето|осень|зима)( +.* +| +)(максимальная|макс\.)( средняя температура)( +.* +| +)(|-)(\d*)°( +.*|))");
+    ifstream fin("input.txt");
+    string str;
+    int i=1;
+    while (getline(fin, str)) {
+        cout <<i++<<")-----------\n";
+        getline(fin, str);
+        cout << "Given string: \"" + str + "\"\n";
+        smatch res;
+        auto begin = str.cbegin();
+        auto end = str.cend();
+        if (regex_match(str, res, r)) {
+            cout << "fits" << '\n';
+            Temp(res);
+        } else {
+            cout << "doesn't fit" << '\n';
+        }
+        if (regex_search(str, res, r)) {
+            cout << "substring: \"" + res.str() + "\" fits\n";
+            Temp(res);
+        } else {
+            cout << "no matches\n";
+        }
+    }
     return 0;
 }
